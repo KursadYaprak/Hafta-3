ae2acceb692dc525197a869f4fc615c0fb338f06
Kürşad Yaprak <kursadyaprak80@hotmail.com>
2023-10-02 02:36:48 +0300

    Mayın Tarlası Oyunu

--- MayinTarlasi/src/MineSweeper.java
+++ MayinTarlasi/src/MineSweeper.java
@@ -3,7 +3,7 @@ import java.util.Random;
 import java.util.Arrays;
 
 public class MineSweeper {
-
+    //5. Madde ---> Proje Burada MineSweeper Sınıfı İçerisinde Tasarlandı.
     int lineNumber, pillarNumber, size;
     int[][] map;
     int[][] board;
@@ -34,14 +34,15 @@ public class MineSweeper {
 
         while(isGame) {
 
-        createBoard(board);
-
+        createBoard(board); //11. Madde ---> Kullanıcı her hamle yaptığında alan güncelleniyor.
+            // 9. Madde ---> Kullanıcıdan işaretlemek istediği satır ve sütun burada alınıyor.
         System.out.print("Bir Satır Seçiniz : ");
         lineChoice = input.nextInt();
         System.out.print("Bir Sütun Seçiniz : ");
         pillarChoice = input.nextInt();
-
+        //10. Madde ---> Kullanıcının seçtiği nokta alanın sınırlarındamı diye kontrol ediliyor. Uyarı mesajı veriliyor. Continue komutu ile döngü tekrar dönüyor ve tekrar giriş kısmı geliyor.
         if (lineChoice < 0 || lineChoice > lineNumber - 1) {
+            //6. Madde ---> Oyunun Başlaması ve ardından kazanma kaybetme durumlarının kontrolleri burada bulunuyor.
             System.out.println("Oyun Alanına Ait Olmayan Bir Konum Girdiniz.");
             continue;
         }
@@ -51,14 +52,14 @@ public class MineSweeper {
         }
         if (!openBoard[lineChoice][pillarChoice]) {
             if (map[lineChoice][pillarChoice] != -1) {
-                checkMine(lineChoice, pillarChoice);
+                checkMine(lineChoice, pillarChoice); //12. Madde ---> Girilen noktanın etrafını kontrol edip mayın sayısını giren veya mayın sayısı 0'sa yerine 0 yazan kısım.
                 rightChoice++;
-                if (rightChoice == (size - (size / 4))) {
+                if (rightChoice == (size - (size / 4))) { //14. Madde ve 15. Madde ---> (14)Tüm noktalar mayınsız seçilirse kazanma kontrolü burada. (15)Kaybetme durumuna uygun mesaj burada.
                     System.out.println("Tebrikler. Tüm Mayınsız Alanları Buldunuz.");
                     createBoard(board);
                     break;
                 }
-            } else {
+            } else { //13. Madde ve 15. Madde ---> (13)Kullanıcı mayına basarsa oyunu bitiren kısım. (15)Kaybetme durumundaki uygun mesaj burada.
                 System.out.println("Mayına Bastınız !\nOyun Bitti.");
                 createBoard(map);
                 isGame = false;
@@ -71,7 +72,7 @@ public class MineSweeper {
 }
 
     public void setMines() {
-
+        //8. Madde ---> Diziye uygun sayıda mayın bu kısımda yerleştiriliyor.
         int randLine, randPillar, mine = 0;
 
         while (mine != (size / 4)) {
